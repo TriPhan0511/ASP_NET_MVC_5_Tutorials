b761c16055884ca48b1de78f1b311d8ca2fda184
Tri Phan <phanphutri@gmail.com>
2022-05-29 20:08:09 +0700

    updated

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -8,12 +8,19 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        // GET: /HelloWorld/
-        public string Index()
-        {
-            return "This is my <b>default</b> action...";
-        }
+        //// GET: /HelloWorld/
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
 
+        // GET: /HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        // GET: /HelloWorld/Welcome/name/id
         public string Welcome(string name, int id)
         {
             return HttpUtility.HtmlEncode($"Hello {name}, ID: {id}");
