c72d8b7e42f6f68ef75ca2a727048ac7c2dbed46
Tri Phan <phanphutri@gmail.com>
2022-06-06 16:53:41 +0700

    updated

--- MvcMovie2/MvcMovie2/Models/Movie.cs
+++ MvcMovie2/MvcMovie2/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie2.Models
 {
@@ -7,6 +8,11 @@ namespace MvcMovie2.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
